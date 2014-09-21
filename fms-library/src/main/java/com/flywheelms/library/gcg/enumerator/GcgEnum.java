b7906969287cc0e96f06708c9742c893ccf2a32c
Steven Stamps <sstamps@flywheelcorporation.com>
2014-09-20 21:57:12 -0500

    incremental - decouple gConGUI from FlywheelMS

--- fms-library/src/main/java/com/flywheelms/library/gcg/enumerator/GcgEnum.java
+++ fms-library/src/main/java/com/flywheelms/library/gcg/enumerator/GcgEnum.java
@@ -1,49 +1,44 @@
 package com.flywheelms.library.gcg.enumerator;
 
-import com.flywheelms.library.gcg.GcgApplication;
-import com.flywheelms.library.gcg.interfaces.GcgPerspective;
-
-import java.util.ArrayList;
-
 /**
- * Just for documentation.
- * Use this as an example for other enum base classes.
+ * Just a "Marker Interface" for documentation.
+ * Use comments in body as an example for GcgEnum implementations.
  */
-public class GcgEnum {
+public interface GcgEnum {
 
-    protected static final ArrayList<GcgEnum> VALUES = new ArrayList<GcgEnum>();
-
-    public static ArrayList<GcgEnum> values() {
-        return GcgEnum.VALUES;
-    }
-
-    public static GcgEnum getObjectForName(String aName) {
-        GcgEnum theGcgEnum = null;
-        for(GcgEnum theInstance : GcgEnum.VALUES) {
-            if(theInstance.getName().equals(aName)) {
-                theGcgEnum = theInstance;
-                break;
-            }
-        }
-        return theGcgEnum;
-    }
-
-    private final int nameResourceId;
-    private String name;
-
-    protected GcgEnum(int aNameResourceId, GcgPerspective[] anGcgPerspectiveArray) {
-        this.nameResourceId = aNameResourceId;
-        this.name = GcgApplication.getAppResources().getString(this.nameResourceId);
-    }
-
-    public int getNameResourceId() {
-        return this.nameResourceId;
-    }
-
-    public String getName() {
-        if(this.name == null) {
-            this.name = GcgApplication.getAppResources().getString(this.nameResourceId);
-        }
-        return this.name;
-    }
+//    protected static final ArrayList<GcgEnum> VALUES = new ArrayList<GcgEnum>();
+//
+//    public static ArrayList<GcgEnum> values() {
+//        return GcgEnum.VALUES;
+//    }
+//
+//    public static GcgEnum getObjectForName(String aName) {
+//        GcgEnum theGcgEnum = null;
+//        for(GcgEnum theInstance : GcgEnum.VALUES) {
+//            if(theInstance.getName().equals(aName)) {
+//                theGcgEnum = theInstance;
+//                break;
+//            }
+//        }
+//        return theGcgEnum;
+//    }
+//
+//    private final int nameResourceId;
+//    private String name;
+//
+//    protected GcgEnum(int aNameResourceId, GcgPerspective[] anGcgPerspectiveArray) {
+//        this.nameResourceId = aNameResourceId;
+//        this.name = GcgApplication.getAppResources().getString(this.nameResourceId);
+//    }
+//
+//    public int getNameResourceId() {
+//        return this.nameResourceId;
+//    }
+//
+//    public String getName() {
+//        if(this.name == null) {
+//            this.name = GcgApplication.getAppResources().getString(this.nameResourceId);
+//        }
+//        return this.name;
+//    }
 }
